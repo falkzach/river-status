a9127404f7412acaf4fc3666b7ceebaac3adab5e
Zachary Falkner <falkzach@gmail.com>
2019-01-23 01:01:38 -0700

    reorganize containers

--- README.md
+++ README.md
@@ -1,18 +1,6 @@
 # River Status and Log
 
-## Development
-Install dependencies
+## Run
 ```
-yarn install
-cd client && yarn install
-```
-
-Run
-```
-yarn dev
-```
-
-## Deployment
-```
-docker-compose up
+docker-compose up --build
 ```
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
-import { async } from 'rxjs/internal/scheduler/async';
 
 class RiverStatus extends React.Component {
     constructor(props) {
