79f56f0f41039e0dcb050449049598c001a30f9d
Zachary Falkner <falkzach@gmail.com>
2019-02-09 00:18:15 -0700

    remove extra mysql connection

--- backend/models/River.js
+++ backend/models/River.js
@@ -1,12 +1,5 @@
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
-  host: 'process.env.MARIADB_HOST',
-  user: 'process.env.MARIADB_USER',
-  password: 'process.env.MARIADB_PASSWORD',
-  database: 'process.env.MARIADB_DATABASE'
-});
-
 class River {
     constructor(params) {
         this.pool = mysql.createPool({
